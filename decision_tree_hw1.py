20e40f002fda7562abca8f5389b1a20d36c58104
tysonbc <tyson.bryant.christensen@gmail.com>
2018-03-15 14:35:05 -0600

    Initial commit

---
+++ decision_tree_hw1.py
@@ -0,0 +1,86 @@
+import typing
+import numpy as np
+
+class DecisionTreeHW(object):
+
+    def __init__(self) -> None:
+        print('ID3 under way...')
+
+    def split(self, feature: list):
+        return {x: (feature==x).nonzero()[0] for x in np.unique(feature)}
+
+    def entropy(s):
+        res = 0
+        val, counts = np.unique(s, return_counts=True)
+        freqs = counts.astype('float')/len(s)
+        for p in freqs:
+            if p != 0.0:
+                res -= p * np.log2(p)
+        return res
+
+if __name__ == '__main__':
+    a = DecisionTreeHW()
+    f = open('training.data', 'r')
+    label = []
+    firstName = []
+    middleName = []
+    lastName = []
+    longFirst = []
+    middle = []
+    sameLetter = []
+    firstBeforeLast = []
+    vowelSecond = []
+    evenLast = []
+    vowels = ['a','e','i','o','u']
+
+
+    while True:
+        line = f.readline()
+        lineData = str.split(line, ' ')
+        if not line or len(lineData) == 1: 
+            break
+        else:
+            
+
+            label.append(lineData[0])
+            firstName.append(lineData[1])
+            lastName.append(lineData[len(lineData)-1])
+            try:
+                if str(lineData[1][1]).lower() in vowels:
+                    vowelSecond.append(1)
+                else:
+                    vowelSecond.append(0)
+            except:
+                vowelSecond.append(0)
+            
+            if len(lineData[len(lineData)-1])%2 == 0:
+                evenLast.append(1)
+            else:
+                evenLast.append(0)
+
+            if lineData[1][0] < lineData[len(lineData)-1][0]:
+                firstBeforeLast.append(1)
+            else:
+                firstBeforeLast.append(0)
+                
+            if lineData[1][0].lower() == lineData[1][len(lineData[1])-1].lower():
+                sameLetter.append(1)
+            else:
+                sameLetter.append(0)
+            if len(lineData[1]) > len(lineData[len(lineData)-1]):
+                longFirst.append(1)
+            else:
+                longFirst.append(0)
+            
+            if len(lineData) == 4:
+                middleName.append(lineData[3])
+                middle.append(1)
+            else:
+                middleName.append('')
+                middle.append(0)
+    
+    b = a.split(feature = longFirst)
+    print(b)
+
+    
+
